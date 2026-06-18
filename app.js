75a733a22cf5e53a4ef9a62ee9dcd67b5cb97aaa
gengliming <gengliming@meituan.com>
2026-06-18 16:09:59 +0800

    fix: use globalThis.Path2D for more reliable polyfill in WeChat

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // app.js
 
-// hanzi-writer 检测到全局 Path2D 时会用 new Path2D(svgPath)，
-// 但微信小程序要求用 canvas.createPath2D()，两者不兼容导致笔划不显示。
-// 清除全局 Path2D，让 hanzi-writer 走直接 canvas 绘制路径（兼容微信 canvas）。
+// hanzi-writer 检测到 global.Path2D 存在时会用 new Path2D(svgPath)，
+// 但微信小程序 type="2d" canvas 的 Path2D 与浏览器 Path2D 不兼容，导致笔划不显示。
+// 在此清除，强制 hanzi-writer 走直接 canvas 绘制路径（moveTo/lineTo/bezierCurveTo）。
 try {
-  // eslint-disable-next-line no-undef
-  if (typeof Path2D !== 'undefined') {
-    global.Path2D = undefined;
-  }
-} catch (e) { /* ignore */ }
+  // globalThis 是 WeChat JS 引擎中的全局对象（同 global / this）
+  Object.defineProperty(globalThis, 'Path2D', {
+    value: undefined,
+    writable: true,
+    configurable: true,
+  });
+} catch (e) {
+  try { globalThis.Path2D = undefined; } catch (e2) { /* ignore */ }
+}
 
 App({})
